e2b7308ab8096b806d1414f61df0a863f8149f95
Lucas Christian <lucaschgf7@gmail.com>
2026-09-15 22:16:46 -0300

    initial commit

---
+++ src/main/java/br/com/medflow/MedflowApplication.java
@@ -0,0 +1,13 @@
+package br.com.medflow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MedflowApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MedflowApplication.class, args);
+	}
+
+}
